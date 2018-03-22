d83b319b9a22b6795a4b6211f65182e3bea6dcac
Lukas T <lukas.trinkunas@gmail.com>
2018-03-22 12:16:25 +0000

    Added spinner

--- app/src/main/java/blah/drunkassistant/MainActivity.java
+++ app/src/main/java/blah/drunkassistant/MainActivity.java
@@ -3,6 +3,11 @@ package blah.drunkassistant;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,7 +15,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Intent intent = new Intent(this, BarMaps.class);
-        startActivity(intent);
+        //Intent intent = new Intent(this, BarMaps.class);
+        //startActivity(intent);
+        List<String> choises = new ArrayList<String>();
+        choises.add("Pub");
+        choises.add("Night Club");
+        choises.add("Bar");
+        Spinner barSelection = (Spinner) findViewById(R.id.spinner);
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this, R.layout.support_simple_spinner_dropdown_item, choises);
+        dataAdapter.setDropDownViewResource(R.layout.support_simple_spinner_dropdown_item);
+        barSelection.setAdapter(dataAdapter);
+
     }
 }
